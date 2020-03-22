dda31203b0d8efdbc71d1b62c42b335764e4bd08
R4ckham <ugo@rougelot.fr>
2020-03-22 04:20:06 +0100

    style fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h1>A propos </h1>
 
     <p style="margin-top: 10%">
-      Bienvenu sur le site de prototypage réaliser bénévolement par les équipes de CA-TS <br>
+      Bienvenu sur le site de prototypage réalisé bénévolement par les équipes de CA-TS <br>
       Le but de cet outil est de fournir à cours termes une liste des commerces ouverts <br>
       et de donner un état des lieux rapides de ces commerces.
     </p>
